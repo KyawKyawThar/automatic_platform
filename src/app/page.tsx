d44b2631767aa51689c7458b1657830e1c0b71ac
KyawKyawThar <kyawkyaw.thar84@gmail.com>
2026-01-07 14:31:31 +0700

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import {Button} from "@/components/ui/button";
-import prisma from "@/lib/db";
-import { json } from "stream/consumers";
+'use-';
 
-async function Home  () {
-  const user = await prisma.user.findMany()
-  return <div className="min-h-screen min-w-screen flex items-center justify-center">
-     {JSON.stringify(user)}
-  </div>
+import { caller, getQueryClient, trpc } from '@/trpc/server';
+import { Client } from './client';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+async function Home() {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUser.queryOptions());
+
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Client />
+      </HydrationBoundary>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
